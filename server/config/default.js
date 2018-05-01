e22c31ebc9a342cedee876a17f40088f6242a077
yonatan.nugraha <yonatan.nugraha@hotmail.com>
2018-05-01 15:45:05 +0700

    add production env

--- server/config/default.js
+++ server/config/default.js
@@ -6,6 +6,7 @@ module.exports = {
     host: 'localhost',
     port: 27017,
     name: 'caffeinehunt',
-    url: 'mongodb://localhost:27017/caffeinehunt',
+    // url: 'mongodb://localhost:27017/caffeinehunt',
+    url: 'mongodb://caffeinemaster:123456@ds225308.mlab.com:25308/caffeinehunt',
   },
 };
